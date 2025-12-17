d1cc53caea510ee3d688b51b74e067616d50127b
Tony Lam <tony.lam@iki.fi>
2025-12-17 10:53:07 +0200

    fix: filter out past dates and disable blacklist that is not working correctly ATM

--- src/lib/unicafeUtils.ts
+++ src/lib/unicafeUtils.ts
@@ -1,5 +1,4 @@
 import type { UnicafeRestaurants } from '../types/unicafeTypes';
-import { checkBlacklist } from './blacklistUtils';
 import { fetchMenu } from './fetchUnicafeMenu';
 
 const getRestaurant = () => {
@@ -37,6 +36,28 @@ const getRestaurant = () => {
   return restaurants;
 };
 
+const checkIfDateIsPast = (dateString: string): boolean => {
+  const today = new Date();
+
+  const formattedDateString = dateString
+    .split(' ')
+    .slice(1)
+    .join(' ')
+    .slice(0, -1);
+  const dateParts = formattedDateString.split('.');
+  const day = parseInt(dateParts[0]);
+  const month = parseInt(dateParts[1]) - 1; // subtract 1 because months are 0-indexed
+  const dateToCheck = new Date(today.getFullYear(), month, day);
+
+  const startOfToday = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  );
+
+  return dateToCheck < startOfToday;
+};
+
 export const checkIfFoodIsInWeekOfRestaurant = (
   searchedFood: string,
   restaurantName: string,
@@ -55,7 +76,7 @@ export const checkIfFoodIsInWeekOfRestaurant = (
     const date = dailyMenu.date;
     dailyMenu.data.forEach((foodItem) => {
       if (foodItem.name.includes(searchedFood)) {
-        if (checkBlacklist(searchedFood, foodItem.name)) {
+        if (checkIfDateIsPast(date)) {
           return;
         }
         datesWhenFoodIsAvailable.push(date);
@@ -77,7 +98,7 @@ export const checkFoodInAllRestaurants = (
   response: UnicafeRestaurants
 ) => {
   const foodAvailability: {
-    [restaurantName: string]: [date: string, food: string][];
+    [restaurantName: string]: [dateString: string, food: string][];
   } = {};
 
   const restaurants = getRestaurant();
