0fee45797b86e004b01339c44300d5be3d6637e7
Tony Lam <tony.lam@iki.fi>
2025-12-22 21:34:58 +0100

    fix: handle year transition in date comparison

--- src/lib/unicafe.utils.ts
+++ src/lib/unicafe.utils.ts
@@ -9,7 +9,18 @@ export const checkIfDateIsPast = (dateString: string): boolean => {
   const dateParts = formattedDateString.split('.');
   const day = parseInt(dateParts[0]);
   const month = parseInt(dateParts[1]) - 1; // subtract 1 because months are 0-indexed
-  const dateToCheck = new Date(today.getFullYear(), month, day);
+
+  // handle year transition
+  const currentMonth = today.getMonth();
+  let year = today.getFullYear();
+  // if the current month is January and the month to check is December, it's last year
+  if (currentMonth === 0 && month === 11) {
+    year = today.getFullYear() - 1;
+  } else if (currentMonth === 11 && month === 0) {
+    year = today.getFullYear() + 1;
+  }
+
+  const dateToCheck = new Date(year, month, day);
 
   const startOfToday = new Date(
     today.getFullYear(),
