80607f1c4397adf2d1ac238ea6c3eaf3ca06a3cf
Tony Lam <tony.lam@iki.fi>
2025-12-26 11:46:57 +0100

    feat: add node-cache for caching unicafe fetch

--- backend/src/lib/fetchUnicafeMenu.ts
+++ backend/src/lib/fetchUnicafeMenu.ts
@@ -1,10 +1,24 @@
-import type { UnicafeRestaurants } from "../types/unicafeTypes";
+import NodeCache from 'node-cache';
+import type { UnicafeRestaurants } from '../types/unicafeTypes';
 
-const apiUrl = "https://unicafe.fi/wp-json/swiss/v1/restaurants/?lang=fi";
+const apiUrl = 'https://unicafe.fi/wp-json/swiss/v1/restaurants/?lang=fi';
+const cache = new NodeCache({ stdTTL: 300 }); // 300 seconds = 5 minutes
+const CACHE_KEY = 'menu';
 
 export const fetchMenu = async () => {
-    const response = await fetch(apiUrl);
-    const data: UnicafeRestaurants =
-        (await response.json()) as UnicafeRestaurants;
-    return data;
+  // Check if data is in cache
+  const cachedData = cache.get<UnicafeRestaurants>(CACHE_KEY);
+  if (cachedData) {
+    return cachedData;
+  }
+
+  // Fetch fresh data
+  const response = await fetch(apiUrl);
+  const data: UnicafeRestaurants =
+    (await response.json()) as UnicafeRestaurants;
+
+  // Store in cache
+  cache.set(CACHE_KEY, data);
+
+  return data;
 };
