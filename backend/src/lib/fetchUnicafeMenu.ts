c138007032a02b862adecbd832d93746883e3036
Tony Lam <tony.lam@iki.fi>
2025-12-26 11:49:34 +0100

    feat: implement frontend

--- backend/src/lib/fetchUnicafeMenu.ts
+++ backend/src/lib/fetchUnicafeMenu.ts
@@ -6,18 +6,15 @@ const cache = new NodeCache({ stdTTL: 300 }); // 300 seconds = 5 minutes
 const CACHE_KEY = 'menu';
 
 export const fetchMenu = async () => {
-  // Check if data is in cache
   const cachedData = cache.get<UnicafeRestaurants>(CACHE_KEY);
   if (cachedData) {
     return cachedData;
   }
 
-  // Fetch fresh data
   const response = await fetch(apiUrl);
   const data: UnicafeRestaurants =
     (await response.json()) as UnicafeRestaurants;
 
-  // Store in cache
   cache.set(CACHE_KEY, data);
 
   return data;
